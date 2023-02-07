aaa91d1774af2c01d1c0ddc6e60658f0c0920fbd
Sai Lokesh Mallisetty <sailokesh.m@deccansoft.net>
2023-02-07 11:10:08 +0530

    Additional changes in demo

--- Repositories/EmployeeRepos.cs
+++ Repositories/EmployeeRepos.cs
@@ -9,18 +9,18 @@ namespace CrudDemoApp.Repositories
 {
     public interface IEmployee
     {
-        Task<List<EmployeeDto>> GetEmployees();
+        Task<PaginationEntityDto<EmployeeDto>> GetEmployees( string? searchtext,int skip,int maxresults);
         Task<EmployeeDto> GetEmployee(int id);
-        Task<bool> UpdateEmployee(EditEmployeeDto editemployeeDto);
-        Task AddEmployee(AddEmployeeDto AddemployeeDto);
-        Task<bool> DeleteEmployee(int id);
+        Task<Responce> UpdateEmployee(EditEmployeeDto editemployeeDto);
+        Task<Responce> AddEmployee(AddEmployeeDto addemployeeDto);
+        Task<Responce> DeleteEmployee(int id);
 
         Task<EmployeeDto> GetEmployeeInfo();
     }
     public class EmployeeRepos : IEmployee
     {
         private readonly EmployeeContext _context;
-        private readonly  ClaimServices _claimServices;
+        private readonly ClaimServices _claimServices;
 
         public EmployeeRepos(EmployeeContext context, ClaimServices claimServices)
         {
@@ -28,8 +28,9 @@ namespace CrudDemoApp.Repositories
             _claimServices = claimServices;
         }
 
-        public async Task AddEmployee(AddEmployeeDto addemployeeDto)
+        public async Task<Responce> AddEmployee(AddEmployeeDto addemployeeDto)
         {
+            var responce = new Responce();
             try
             {
                 var employee = new Employee()
@@ -46,15 +47,18 @@ namespace CrudDemoApp.Repositories
                 };
                 await _context.employees.AddAsync(employee);
                 await _context.SaveChangesAsync();
+                return responce;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                responce.ErrorMessage = ex.Message;
+                return responce;
             }
         }
 
-        public async Task<bool> DeleteEmployee(int id)
+        public async Task<Responce> DeleteEmployee(int id)
         {
+            var responce = new Responce();
             try
             {
                 var employee = await _context.employees.FirstOrDefaultAsync(x => x.Id == id);
@@ -62,17 +66,18 @@ namespace CrudDemoApp.Repositories
                 {
                     employee.IsActive = false;
                     employee.IsDelete = true;
-                    return true;
+                    return responce;
                 }
                 else
                 {
-                    return false;
+                    responce.ErrorMessage = "Data Not Found";
+                    return responce;
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                responce.ErrorMessage = ex.Message;
+                return responce;
             }
 
         }
@@ -93,20 +98,16 @@ namespace CrudDemoApp.Repositories
                 }).FirstOrDefaultAsync();
                 if (employee != null)
                 {
-
                     return employee;
                 }
                 else
                 {
-                    return null;
+                    throw new ApplicationException("Data not found");
                 }
-
-
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                throw new ApplicationException(ex.Message);
             }
         }
 
@@ -114,41 +115,30 @@ namespace CrudDemoApp.Repositories
         {
             try
             {
-                 int id = _claimServices.GetCurrentUserId();
-                var employee = await _context.employees.Where(x => x.Id == id && x.IsActive && !x.IsDelete).Select(x => new EmployeeDto()
-                {
-                    Id = x.Id,
-                    Name = x.Name,
-                    Email = x.Email,
-                    Password = x.Password,
-                    FatherName = x.FatherName,
-                    Age = x.Age,
-                    Address = x.Address
-                }).FirstOrDefaultAsync();
+                int id = _claimServices.GetCurrentUserId();
+
+                var employee = await GetEmployee(id); 
+
                 if (employee != null)
                 {
-
                     return employee;
                 }
                 else
                 {
                     return null;
                 }
-
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
 
-        public async Task<List<EmployeeDto>> GetEmployees()
+        public async Task<PaginationEntityDto<EmployeeDto>> GetEmployees(string? searchtext, int skip, int maxresults)
         {
             try
             {
-
-                var employees = await _context.employees.Select(x => new EmployeeDto
+                var employees = await _context.employees.Where(x=>x.IsActive && !x.IsDelete).Select(x => new EmployeeDto
                 {
                     Id = x.Id,
                     Name = x.Name,
@@ -158,43 +148,69 @@ namespace CrudDemoApp.Repositories
                     Age = x.Age,
                     Address = x.Address
                 }).AsNoTracking().ToListAsync();
-                return employees;
 
+                if(employees != null)
+                {
+                    if(!string.IsNullOrEmpty(searchtext) && !string.IsNullOrWhiteSpace(searchtext))
+                    {
+                       var res = employees.Where(x => x.Name.ToLower().Contains(searchtext.ToLower()) || x.Email.ToLower().Contains(searchtext.ToLower())).ToList();
+
+                        var data = new PaginationEntityDto<EmployeeDto>();
+                        data.Count = res.Count();
+                        data.Entities = res.OrderByDescending(x=>x.Id).Skip(skip).Take(maxresults).ToList();
+
+                        return data;
+
+                    }
+                    else
+                    {
+                        var data = new PaginationEntityDto<EmployeeDto>();
+                        data.Count = employees.Count();
+                        data.Entities = employees.OrderByDescending(x => x.Id).Skip(skip).Take(maxresults).ToList();
+                        return data;
+                    }
+                }
+                else
+                {
+                    throw new ApplicationException("Data not found");
+                }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                throw new ApplicationException(ex.Message);
             }
         }
 
-        public async Task<bool> UpdateEmployee(EditEmployeeDto editemployeeDto)
+        public async Task<Responce> UpdateEmployee(EditEmployeeDto editemployeeDto)
         {
             var responce = new Responce();
             try
             {
                 var employee = await _context.employees.FirstOrDefaultAsync(x => x.Id == editemployeeDto.Id);
 
-
-
-                employee.Name = editemployeeDto.Name;
-                employee.Email = editemployeeDto.Email;
-                employee.Password = editemployeeDto.Password;
-                employee.FatherName = editemployeeDto.FatherName;
-                employee.Age = editemployeeDto.Age;
-                employee.Address = editemployeeDto.Address;
-                employee.IsDelete = false;
-                employee.IsActive = true;
-
-
-                await _context.SaveChangesAsync();
-
-                return responce.IsSuccess;
+                if (employee != null)
+                {
+                    employee.Name = editemployeeDto.Name;
+                    employee.Email = editemployeeDto.Email;
+                    employee.Password = editemployeeDto.Password;
+                    employee.FatherName = editemployeeDto.FatherName;
+                    employee.Age = editemployeeDto.Age;
+                    employee.Address = editemployeeDto.Address;
+                    employee.IsDelete = false;
+                    employee.IsActive = true;
+                    await _context.SaveChangesAsync();
+                    return responce;
+                }
+                else
+                {
+                    responce.ErrorMessage = "Data not found";
+                    return responce;
+                }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                responce.ErrorMessage = ex.Message;
+                return responce;
             }
         }
     }
